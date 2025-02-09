028c3c30d1d55a6b4c7877b1ff3ccb6279308ce8
kuurong <s10y06@naver.com>
2025-02-09 18:27:49 +0900

    Link 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <header>
+          <Link href={"/"}>인덱스</Link>
+          &nbsp;
+          <Link href={"/search"}>서치</Link>
+          &nbsp;
+          <Link href={"/book/1"}>book/1</Link>
+          &nbsp;
+        </header>
         {children}
       </body>
     </html>
